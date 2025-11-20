bc2fa6f8843690018382d4478f5252fd4a35dff9
Leonardo Lucas da Silva <leonardo3443@gmail.com>
2025-11-20 20:19:39 -0300

    Fix: Changed first tests to a file referring to preliminary usage

---
+++ algorithms/preliminar_tests.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+from algorithms.arnoldi import arnoldi
+from algorithms.lanczos import lanczos
+from algorithms.power_method import power_method
+from algorithms.qr_iteration import qr_iteration_householder
+from algorithms.jacobi_ciclic import jacobi_cyclic
+
+A = np.array([
+    [19, 22, 19, 28, 34],
+    [22, 32, 28, 37, 41],
+    [19, 28, 44, 41, 51],
+    [28, 37, 41, 52, 56],
+    [34, 41, 51, 56, 77]
+], dtype=float)
+
+print("Eigenpairs by Python Library:")
+print(np.linalg.eig(A))
+
+print("Arnoldi method:")
+
+B = A.copy()
+v1 = np.ones(5)
+H,Q = arnoldi(B,v1,B.shape[0])
+
+print(H)
+print(Q.T)
+
+print("QR iteration:")
+
+B = A.copy()
+
+H,Q = qr_iteration_householder(B)
+print(H)
+print(Q)
+
+print("Jacobi cyclic:")
+
+B = A.copy()
+
+A,V = jacobi_cyclic(B)
+
+print(A)
+print(V)
+
+print("Lanczos:")
+
+A = np.array([
+    [11, 6, 9, 10],
+    [6, 7, 7, 9],
+    [9, 7, 9, 10],
+    [10, 9, 10,14]
+], dtype=float)
+
+B = A.copy()
+print(np.linalg.eig(B))
+v1 = np.random.rand(4)
+
+eigenvalues, eigenvectors = lanczos(B,v1, B.shape[0], B.shape[0])
+
+print(eigenvalues)
+print(eigenvectors)
