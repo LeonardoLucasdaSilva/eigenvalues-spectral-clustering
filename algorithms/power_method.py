e12b0326adcadd525c6cefce5e466cd66e410445
Leonardo Lucas da Silva <leonardo3443@gmail.com>
2025-11-23 16:30:51 -0300

    Feat: LU in shift power method

--- algorithms/power_method.py
+++ algorithms/power_method.py
@@ -1,6 +1,7 @@
 import numpy as np
+from scipy.linalg import lu_factor, lu_solve
 
-def power_method(A, q0, maxiter = 1000, tol = 1e-12):
+def power_method(A, q0, maxiter = 500, tol = 1e-12):
     """
     Find the dominant eigenvalue of a matrix by the power method.
 
@@ -39,7 +40,9 @@ def power_method(A, q0, maxiter = 1000, tol = 1e-12):
         # Updates the last iteration value
         nu[0] = nu[1]
 
-def shifted_inverse_power_method(A, q0, mu, maxiter=1000, tol=1e-12):
+    return nu[1], q
+
+def shifted_inverse_power_method(A, q0, mu, maxiter=1000, tol=1e-10):
     """
     Find the eigenvalue of A closest to a given shift mu using the shifted inverse power method.
 
@@ -55,6 +58,14 @@ def shifted_inverse_power_method(A, q0, mu, maxiter=1000, tol=1e-12):
         q: corresponding eigenvector
     """
 
+    n = A.shape[0]
+
+    # Precompute shifted matrix ONCE
+    M = A - mu * np.eye(n)
+
+    # LU factorization
+    lu, piv = lu_factor(M)
+
     q = q0 / np.linalg.norm(q0)
     nu = np.zeros(2)
 
@@ -64,7 +75,7 @@ def shifted_inverse_power_method(A, q0, mu, maxiter=1000, tol=1e-12):
     for k in range(maxiter):
 
         # Solve (A - muI) z = q  instead of multiplying
-        z = np.linalg.solve(A - mu * np.eye(A.shape[0]), q)
+        z = lu_solve((lu, piv), q)
 
         # Normalize
         q = z / np.linalg.norm(z)
