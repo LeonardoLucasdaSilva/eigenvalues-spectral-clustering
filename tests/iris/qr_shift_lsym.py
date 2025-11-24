d01868f917de31aba89047ae282aa08248edc4e8
Leonardo Lucas da Silva <leonardo3443@gmail.com>
2025-11-23 23:32:09 -0300

    Feat: Tests for the iris dataset using QR shift methods

---
+++ tests/iris/qr_shift_lsym.py
@@ -0,0 +1,32 @@
+import numpy as np
+from algorithms.qr_iteration import qr_iteration_householder_shift
+from algorithms.retrieve_data import matrix_L_sym
+from utils.metrics import relative_error
+import time
+
+# Build the L_sym matrix
+L_sym = matrix_L_sym(True, 0.01, 'iris')
+
+# Retrieve eigenvalues using numpy for comparison
+r1 = np.linalg.eigvals(L_sym)
+times = []
+
+# Number of samples to average time
+n = 1
+
+print("QR iteration with shift for L_sym matrix:")
+for k in range(n):
+
+    start = time.time()
+
+    H,Q = qr_iteration_householder_shift(L_sym, tol = 1e-8)
+
+    end = time.time()
+
+    # Retrieve eigenvalues from diagonal
+    r2 = np.diag(H)
+
+    times.append(end - start)
+
+print(f"Average processing time with {n} samples: {np.mean(times)} seconds")
+relative_error(r1,r2)
